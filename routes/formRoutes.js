3f3b218713ab82ecd809d5d49769ab4cc6bd36aa
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-01-29 19:56:08 +0500

    refactor: streamline form route definitions for improved readability

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -4,46 +4,17 @@ import {
   getAllForms,
   getFormById,
   updateForm,
-  deleteForm
+  deleteForm,
 } from "../controllers/formController.js";
 
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
-/* =========================
-   ROUTES
-========================= */
-
-// create form
-router.post(
-  "/",
-  upload.array("images", 5),
-  createForm
-);
-
-// get all forms
-router.get(
-  "/",
-  getAllForms
-);
-
-// get single form
-router.get(
-  "/:id",
-  getFormById
-);
-
-// update form
-router.put(
-  "/:id",
-  updateForm
-);
-
-// delete form
-router.delete(
-  "/:id",
-  deleteForm
-);
+router.post("/", upload.array("images", 5), createForm);
+router.get("/", getAllForms);
+router.get("/:id", getFormById);
+router.put("/:id", updateForm);
+router.delete("/:id", deleteForm);
 
 export default router;
