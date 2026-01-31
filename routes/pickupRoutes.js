9072b6465bffa2334ad8b7f3e5cbe534060d62e1
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-01-31 15:14:20 +0500

    feat: add pickup routes for CRUD operations on PickUpDetails

---
+++ routes/pickupRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createPickUpDetails, getAllPickUpDetails, getPickUpDetailsById, deletePickUpDetails, updatePickUpDetails } from "../controllers/pickUpController";
+
+const router = express.Router();
+router.post("/", createPickUpDetails);
+router.get("/", getAllPickUpDetails);
+router.get("/:id", getPickUpDetailsById);
+router.put("/:id", updatePickUpDetails);
+router.delete("/:id", deletePickUpDetails);
+
+export default router;
