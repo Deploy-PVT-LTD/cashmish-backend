4dfbee70e4bd95d5bf7eb966557083c77c078da4
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-02-02 18:59:03 +0500

    feat: add getUserById function to retrieve a single user by ID

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,6 +64,16 @@ export const updateUserRole = async (req, res) => {
     res.status(500).json({ message: 'Server error', error: err });
   }
 };
+//get single user
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+};
 //delete user
 export const deleteUser = async (req, res) => {
   try {
