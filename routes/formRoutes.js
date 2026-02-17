19d9f2fb76e5b3c580c0332807425b57a23182f1
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-02-17 14:13:15 +0500

    feat: fix comments in mobile routes

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -9,8 +9,7 @@ import {
   getEstimate,
 } from "../controllers/formController.js";
 import upload from "../middleware/upload.js";
-import jwt from "jsonwebtoken";
-import keys from "../config/keys.js";
+
 
 const router = express.Router();
 
--- routes/mobileRoute.js
+++ routes/mobileRoute.js
@@ -5,21 +5,20 @@ import passport from "passport";
 const router = express.Router();
 const requireAuth = passport.authenticate("jwt", { session: false });
 
-// 1. Post hamesha top par rakh sakte hain
 router.post("/", requireAuth, addMobile);
 
-// 2. GET ALL
+//  GET ALL
 router.get("/", getMobiles);
 
-// 3. BRAND wala route ID se upar hona chahiye (ZAROORI HAI)
+// GET BRANDS
 router.get("/brand", getMobilesByBrand);
 
-// 3.5 REQUESTS (Super Admin)
+// REQUESTS (Super Admin)
 router.get("/requests", requireAuth, getMobileRequests);
 router.post("/requests/:id/approve", requireAuth, approveRequest);
 router.post("/requests/:id/reject", requireAuth, rejectRequest);
 
-// 4. ID wala route sabse niche hona chahiye
+// ID Routes
 router.get("/:id", getMobileById);
 router.put("/:id", requireAuth, updateMobile);
 router.delete("/:id", requireAuth, deleteMobile);
